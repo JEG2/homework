24863844e9a207f33845767483303c42adddba93
Clayton Flesher <claytonflesher@gmail.com>
2015-01-18 13:49:12 -0600

    new objects and Runner

--- Fiasco/io_fiasco.rb
+++ Fiasco/io_fiasco.rb
@@ -1,4 +1,4 @@
-class Pull_file
+class Build_tables
   def initialize(file_name1, file_name2)
     @file_name1 = file_name1
     @file_name2 = file_name2
@@ -10,7 +10,7 @@ class Pull_file
     return array
   end
 
-  def build_subcategories
+  def run
     keys      = build_categories
     hash      = Hash.new
     keys.each { |i| File.open(@file_name2, "r") do |file|
@@ -23,9 +23,42 @@ class Pull_file
     }
     return hash
   end
+end
+
+relationships = Build_tables.new("relationship_categories", "relationship_subcategories").run
+
+needs         = Build_tables.new("needs_categories", "needs_subcategories").run
+
+objects       = Build_tables.new("object_categories", "object_subcategories").run
+
+locations     = Build_tables.new("location_categories", "location_subcategories").run
+
+class Runner
+  def initilize(relationships, needs, locations, objects)
+    @relationships  = relationships
+    @needs          = needs
+    @locations      = locations
+    @objects        = objects
+    @dice           = Array.new(16) { rand(1..6) }
+  end
+
+  def run
+    details = [
+      @needs,
+      @locations,
+      @objects,
+      [@locations, @objects].sample
+    ].shuffle
+    puts "Your dice are #{@dice.join(", ")}"
+    details.each_with_index do |subcategory, i|
+      d = i * 4
+      puts "Player #{i + 1} and Player #{(i + 1) % 4 + 1}:"
+      puts "  #{@relationships.keys[@dice[d] - 1]} (#{@dice[d]}) #{@relationships[@relationships.keys[@dice[d] - 1]][@dice[d + 1] - 1]} (#{@dice[d + 1]})"
+      puts "#{subcategory.keys[@dice[d + 2] - 1]} (#{@dice[d + 2]}) #{subcategory[subcategory.keys[@dice[d] - 1]][@dice[d + 3] - 1]} (#{@dice[d + 3]})"
+      puts
+    end
+  end
 
 end
 
-relationships = Pull_file.new("relationship_categories", "relationship_subcategories")
-p relationships.build_categories
-p relationships.build_subcategories
+Runner.new.run
