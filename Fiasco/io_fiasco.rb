570748356d788164af10fde15e1c366945bbd20f
Clayton Flesher <claytonflesher@gmail.com>
2015-01-18 23:37:44 -0600

    working code

--- Fiasco/io_fiasco.rb
+++ Fiasco/io_fiasco.rb
@@ -1,4 +1,4 @@
-class Build_tables
+class Tables
   def initialize(file_name1, file_name2)
     @file_name1 = file_name1
     @file_name2 = file_name2
@@ -25,16 +25,16 @@ class Build_tables
   end
 end
 
-relationships = Build_tables.new("relationship_categories", "relationship_subcategories").run
+relationships = Tables.new("relationship_categories", "relationship_subcategories").run
 
-needs         = Build_tables.new("needs_categories", "needs_subcategories").run
+needs         = Tables.new("needs_categories", "needs_subcategories").run
 
-objects       = Build_tables.new("object_categories", "object_subcategories").run
+objects       = Tables.new("object_categories", "object_subcategories").run
 
-locations     = Build_tables.new("location_categories", "location_subcategories").run
+locations     = Tables.new("location_categories", "location_subcategories").run
 
 class Runner
-  def initilize(relationships, needs, locations, objects)
+  def initialize(relationships, needs, locations, objects)
     @relationships  = relationships
     @needs          = needs
     @locations      = locations
@@ -54,11 +54,11 @@ class Runner
       d = i * 4
       puts "Player #{i + 1} and Player #{(i + 1) % 4 + 1}:"
       puts "  #{@relationships.keys[@dice[d] - 1]} (#{@dice[d]}) #{@relationships[@relationships.keys[@dice[d] - 1]][@dice[d + 1] - 1]} (#{@dice[d + 1]})"
-      puts "#{subcategory.keys[@dice[d + 2] - 1]} (#{@dice[d + 2]}) #{subcategory[subcategory.keys[@dice[d] - 1]][@dice[d + 3] - 1]} (#{@dice[d + 3]})"
+      puts "#{subcategory.keys[@dice[d + 2] - 1]} (#{@dice[d + 2]}) #{subcategory[subcategory.keys[@dice[d + 2] - 1]][@dice[d + 3] - 1]} (#{@dice[d + 3]})"
       puts
     end
   end
 
 end
 
-Runner.new.run
+Runner.new(relationships, needs, locations, objects).run
