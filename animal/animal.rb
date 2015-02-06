cdf3633dd758bb17878c2b882e7c1c8e9c108c1c
Clayton Flesher <claytonflesher@gmail.com>
2015-02-05 19:30:00 -0600

    placed everything inside of class objects

--- animal/animal.rb
+++ animal/animal.rb
@@ -63,122 +63,135 @@ class Builder
   end
 end
 
-def yes(animals, master_tables, question)
-  animals.delete_if { |animal| !master_tables[question].include?(animal) }
-  return animals
-end
+class Runner
+  def initialize(animals, master_list, table, master_tables)
+    @animals        = animals
+    @master_list    = master_list
+    @table          = table
+    @master_tables  = master_tables
+  end
+    
+  def yes(question)
+    @animals.delete_if { |animal| !@master_tables[question].include?(animal) }
+    return @animals
+  end
 
-def no(animals, master_tables, question)
-  animals.delete_if { |animal| master_tables[question].include?(animal) }
-  return animals
-end
+  def no(question)
+    @animals.delete_if { |animal| @master_tables[question].include?(animal) }
+    return @animals
+  end
 
-def question_loop(animals, master_tables, table, answer_tracking)
-    while animals.size > 1
-      question = table["questions"].sample
+  def question_loop(answer_tracking)
+    while @animals.size > 1
+      question = @table["questions"].sample
       p question
-      table["questions"].delete(question)
+      @table["questions"].delete(question)
       answer = gets.strip
       answer_tracking[question] = answer
       if answer == "yes"
-        animals = yes(animals, master_tables, question)
+        @animals = yes(question)
       elsif answer == "no"
-        animals = no(animals, master_tables, question)
+        @animals = no(question)
       else
         puts "Oops! I didn't understand your response. Please start again."
         exit
       end
     end
-    puts "Is your animal a(n) #{animals.last}?"
+    puts "Is your animal a(n) #{@animals.last}?"
     return answer_tracking
-end
-
-def oops
-  puts "Oops! I didn't understand your response. Please start again."
-  exit
-end
-
-def send_questions_data(help)
-  file_name = "file#{rand(1..99999)}"
-  File.open("data/questions.txt", "a") do |f|
-    f.puts
-    f.puts help["question"]
-    while File.exist?("data/#{file_name}")
-      file_name = "file#{rand(1..99999)}"
-    end
-    f.puts file_name
   end
-  return file_name
+
+  def oops
+    puts "Oops! I didn't understand your response. Please start again."
+    exit
+  end
+
+  def send_questions_data(help)
+    file_name = "file#{rand(1..99999)}"
+    File.open("data/questions.txt", "a") do |f|
+      f.puts
+      f.puts help["question"]
+      while File.exist?("data/#{file_name}")
+        file_name = "file#{rand(1..99999)}"
+      end
+      f.puts file_name
+    end
+    return file_name
+  end
+
+  def run
+    answer_tracking = Hash.new
+    puts "Think of an animal."
+    puts "Please answer all questions with 'yes' or 'no'."
+    puts "Is your animal a category, such as horse, cat, mouse? (As opposed to a particular breed or species)"
+    category = gets.strip
+    if category == "yes"
+      answer_tracking["category"] = category
+      @animals.delete_if { |animal| !@table["category"].include?(animal) }
+      answer_tracking = question_loop(answer_tracking)    
+    elsif category == "no"
+      answer_tracking["category"] = category
+      puts "Is it a breed?"
+      breed = gets.strip
+      if breed == "yes"
+        answer_tracking["breed"] = breed
+        @animals.delete_if { |animal| !@table["breed"].include?(animal) }
+        answer_tracking = question_loop(answer_tracking)
+      elsif breed == "no"
+        answer_tracking["breed"] = breed
+        @animals.delete_if { |animal| !@table["species"].include?(animal) }
+        answer_tracking = question_loop(answer_tracking)
+      else
+        oops
+      end
+    else
+      oops
+    end
+    answer = gets.strip
+    if answer == "yes"
+      puts "Sweet. Thanks for playing."
+    elsif answer == "no"
+      puts "Aw, <expletive deleted>. How about you help me out so I can get smarter. It'll only take a minute."
+      help = Hash.new
+      puts "What kind of animal where you thinking of?"
+      animal = gets.strip
+      help["animal"] = animal
+      puts "Please type a question that has a 'yes' or 'no' answer that might help distinguish your animal from the one I guessed."
+      question = gets.strip
+      help["question"] = question
+      puts "Thanks for playing."
+      p answer_tracking
+      file_name = send_questions_data(help)
+      check_locations = Builder.new
+      check_locations = check_locations.compare
+      if answer_tracking["category"] == "yes"
+        File.open("data/category.txt", "a") { |f| f.puts help["animal"] }
+      elsif
+        answer_tracking["breed"] == "yes"
+        File.open("data/breed.txt", "a") { |f| f.puts help["animal"] }
+      else
+        File.open("data/species.txt", "a") { |f| f.puts help["animal"] }
+      end
+      answer_tracking.each do |question, answer|
+        if answer == "yes"
+          File.open("data/#{check_locations[question]}.txt", "a") { |f| f.puts help["animal"] }
+        end
+      end
+      File.open("data/animal.txt", "a") { |f| f.puts help["animal"] }
+      File.open("data/directory.txt", "a") { |f| f.puts file_name }
+      File.open("data/#{file_name}.txt", "a") { |f| f.puts help["animal"] }
+    else
+      oops
+    end
+  end
 end
 
 animals         = Builder.new
 animals         = animals.pull("animal")
+master_list     = animals
 table           = Builder.new
 table           = table.run
 master_tables   = Builder.new
 master_tables   = master_tables.tag_questions(table)
-
-answer_tracking = Hash.new
-puts "Think of an animal."
-puts "Please answer all questions with 'yes' or 'no'."
-puts "Is your animal a category, such as horse, cat, mouse? (As opposed to a particular breed or species)"
-category = gets.strip
-if category == "yes"
-  answer_tracking["category"] = category
-  animals.delete_if { |animal| !table["category"].include?(animal) }
-  p animals
-  answer_tracking = question_loop(animals, master_tables, table, answer_tracking)
-elsif category == "no"
-  answer_tracking["category"] = category
-  puts "Is it a breed?"
-  breed = gets.strip
-  if breed == "yes"
-    answer_tracking["breed"] = breed
-    animals.delete_if { |animal| !table["breed"].include?(animal) }
-    answer_tracking = question_loop(animals, master_tables, table, answer_tracking)
-  elsif breed == "no"
-    answer_tracking["breed"] = breed
-    animals.delete_if { |animal| ! table["species"].include?(animal) }
-    answer_tracking = question_loop(animals, master_tables, table, answer_tracking)
-  else
-    oops
-  end
-else
-  oops
-end
-answer = gets.strip
-if answer == "yes"
-  puts "Sweet. Thanks for playing."
-elsif answer == "no"
-  puts "Aw, <expletive deleted>. How about you help me out so I can get smarter. It'll only take a minute."
-  help = Hash.new
-  puts "What kind of animal where you thinking of?"
-  animal = gets.strip
-  help["animal"] = animal
-  puts "Please type a question that has a 'yes' or 'no' answer that might help distinguish your animal from the one I guessed."
-  question = gets.strip
-  help["question"] = question
-  puts "Thanks for playing."
-  p answer_tracking
-  file_name = send_questions_data(help)
-  check_locations = Builder.new
-  check_locations = check_locations.compare
-  if answer_tracking["category"] == "yes"
-    File.open("data/category.txt", "a") { |f| f.puts help["animal"] }
-  elsif
-    answer_tracking["breed"] == "yes"
-    File.open("data/breed.txt", "a") { |f| f.puts help["animal"] }
-  else
-    File.open("data/species.txt", "a") { |f| f.puts help["animal"] }
-  end
-  answer_tracking.each do |question, answer|
-    if answer == "yes"
-      File.open("data/#{check_locations[question]}.txt", "a") { |f| f.puts help["animal"] }
-    end
-  end
-  File.open("data/animal.txt", "a") { |f| f.puts help["animal"] }
-  File.open("data/directory.txt", "a") { |f| f.puts file_name }
-  File.open("data/#{file_name}.txt", "a") { |f| f.puts help["animal"] }
-else
-  oops
-end
+guess          = Runner.new(animals, master_list, table, master_tables)
+guess.run
